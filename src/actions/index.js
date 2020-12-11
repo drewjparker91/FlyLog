f80392e99a6fc42fbb668f6c7b51e71454808932
drewparker <drewjparker91@gmail.com>
2020-12-11 11:56:34 -0800

    add content to actions/ActionTypes.js and actions/index.js

---
+++ src/actions/index.js
@@ -0,0 +1,43 @@
+import * as a from './ActionTypes';
+
+export const addLog = (log) => {
+  const{author, species, length, weight, fly, waterCond, weather, createdAt, id} = log;
+  return {
+    type:a.ADD_LOG,
+    author: author,
+    species: species, 
+    length: length,
+    weight: weight,
+    fly: fly,
+    waterCond: waterCond,
+    weather: weather,
+    createdAt: createdAt,
+    id: id
+  }
+}
+
+export const deleteLog = id => ({
+  type: a.DELETE_LOG,
+  id
+})
+
+export const toggleEditForm = () => ({
+  type: a.TOGGLE_EDIT_FORM
+});
+
+export const toggleNewForm = () => ({
+  type: a.TOGGLE_NEW_FORM
+});
+
+export const selectedLog = (log) => {
+  return {
+    type: a.SELECTED_LOG,
+    selectedLog: log
+  }
+}
+
+export const unselectLog = () => ({
+  type: a.UNSELECT_LOG
+})
+
+
