eb232a76b6ed6cf5d3739a6a68f8b95ca5aaacf5
drewparker <drewjparker91@gmail.com>
2020-12-18 10:06:56 -0800

    update src/index.js to handle firebase

--- src/index.js
+++ src/index.js
@@ -7,16 +7,27 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+import 'firebase/auth';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
-store.subscribe(() =>
-  console.log(store.getState())
-)
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users"
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
