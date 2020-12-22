6278ef36a5df631ede73189ea6b24645c00cbd1b
drewparker <drewjparker91@gmail.com>
2020-12-21 16:59:13 -0800

    sign out button moved into header and styled

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -9,11 +9,22 @@ import { useState, useRef } from 'react';
 
 function SignOut() { //sign out function
 
+  const signOutButton = {
+    color: 'white',
+    backgroundColor: '#2b473c',
+    fontSize: '20px',
+    padding: '10px 10px',
+    borderRadius: '5px',
+    margin: '10px 0px',
+    cursor: 'pointer',
+    width: 'auto',
+  }
   const auth = firebase.auth();
 
   return auth.currentUser && (
-
-    <button onClick={() => auth.signOut()}>Sign Out</button>
+    <div style={signOutButton}>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    </div>
   )
 }
 
